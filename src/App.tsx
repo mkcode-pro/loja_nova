298686fb64f70390aa677cc247a7fa2e5caadbc2
[dyad] <git@dyad.sh>
2025-08-11 21:56:38 -0300

    [dyad] Corrigindo a rolagem da página na navegação - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BrandProducts from "./pages/BrandProducts";
 import CategoryProducts from "./pages/CategoryProducts";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/marcas/:brandName" element={<BrandProducts />} />
